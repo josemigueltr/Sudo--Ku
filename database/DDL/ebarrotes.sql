8c2440b14d080ff9e1f0379af974b070b0857ce7
josemigueltr <josemigueltr@ciencias.unam.mx>
2021-07-16 20:43:10 -0500

    Base de datos agregada.

--- database/DDL/ebarrotes.sql
+++ database/DDL/ebarrotes.sql
@@ -16,9 +16,9 @@ USE `ebarrotes`;
 DROP TABLE IF EXISTS `comprador`;
 
 CREATE TABLE comprador (
-	username varchar(100) NOT NULL,
-	correo varchar(100) NOT NULL,
-	telefono varchar(100) NOT NULL,
+	username varchar(20) NOT NULL,
+	correo varchar(20) NOT NULL,
+	telefono varchar(15) NOT NULL,
 	constrasenia varchar(100) NOT NULL,
 	CONSTRAINT Comprador_PK PRIMARY KEY (username)
 )
@@ -34,9 +34,9 @@ COLLATE=utf8mb4_0900_ai_ci;
 DROP TABLE IF EXISTS `vendedor`;
  
 CREATE TABLE vendedor (
-	username varchar(100) NOT NULL,
-	correo varchar(100) NOT NULL,
-	telefono varchar(100) NOT NULL,
+	username varchar(20) NOT NULL,
+	correo varchar(20) NOT NULL,
+	telefono varchar(15) NOT NULL,
 	constrasenia varchar(100) NOT NULL,
 	CONSTRAINT Vendedor_PK PRIMARY KEY (username)
 )
@@ -54,10 +54,10 @@ DROP TABLE IF EXISTS `datos_de_envio`;
 
 CREATE TABLE datos_de_envio (
 	id_datos_envio INT auto_increment NOT NULL,
-	estado varchar(100) NOT NULL,
-	cp varchar(100) NOT NULL,
-	direccion varchar(100) NOT NULL,
-	entre_calles varchar(100) NOT NULL,
+	estado varchar(10) NOT NULL,
+	cp varchar(10 NOT NULL,
+	direccion varchar(30) NOT NULL,
+	entre_calles varchar(30) NOT NULL,
 	CONSTRAINT datos_de_envio_PK PRIMARY KEY (id_datos_envio)
 )
 ENGINE=InnoDB
@@ -73,7 +73,7 @@ DROP TABLE IF EXISTS orden;
 
 CREATE TABLE orden (
 	id_orden INT auto_increment NOT NULL,
-	username varchar(100) NOT NULL,
+	username varchar(20) NOT NULL,
 	id_datos_envio INT NOT NULL,
 	total FLOAT NOT NULL,
 	CONSTRAINT orden_PK PRIMARY KEY (id_orden),
@@ -94,8 +94,8 @@ DROP TABLE IF EXISTS producto;
 
 CREATE TABLE producto (
 	id_producto INT auto_increment NOT NULL,
-	username varchar(100) NOT NULL,
-	nombre varchar(100) NOT NULL,
+	username varchar(20) NOT NULL,
+	nombre varchar(20) NOT NULL,
 	descripcion TEXT NOT NULL,
 	precio FLOAT NOT NULL,
 	calificacion INT NOT NULL,
@@ -137,18 +137,19 @@ DROP TABLE IF EXISTS opinion;
 
 CREATE TABLE opinion (
 	id_opinion INT auto_increment NOT NULL,
-	username varchar(100) NOT NULL,
+	username varchar(20) NOT NULL,
 	id_producto INT NOT NULL,
 	valoracion INT NOT NULL,
 	contenido TEXT NOT NULL,
 	fecha DATE NOT NULL,
 	CONSTRAINT opinion_PK PRIMARY KEY (id_opinion),
-	CONSTRAINT opinion_FK FOREIGN KEY (username) REFERENCES ebarrotes.comprador(username),
-	CONSTRAINT opinion_FK_1 FOREIGN KEY (id_producto) REFERENCES ebarrotes.producto(id_producto)
+	CONSTRAINT opinion_FK FOREIGN KEY (username) REFERENCES comprador(username),
+	CONSTRAINT opinion_FK_1 FOREIGN KEY (id_producto) REFERENCES  producto(id_producto)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_0900_ai_ci;
+
 
 
 
