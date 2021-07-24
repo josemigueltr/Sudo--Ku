7059786d3551063f2b8830bfa133016fa0ff3b6d
eragond <eragond@ciencias.unam.mx>
2021-07-24 02:25:11 -0500

    Definido modelo de opinion. Es necesario crear las relaciones desde comprador y producto.

---
+++ back/models/opinion.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from models.conexion_bd import Base
+
+class Opinion(Base):
+    """Modelo para opinion."""
+    __tablename__ = 'opinion'
+    id_opinion = Column(Integer, primary_key = True) 
+    username = Column(String(20), ForeignKey('comprador.username')) 
+    id_producto = Column(Integer, ForeignKey('producto.id_producto'))
+    valoracion = Column(Integer)
+    contenido = Column(String)
+    fecha = Column(Date) 
+
+    """Redefinicion del metodo constructor predefinido por sqlalchemy"""
+    def __init__(self, valoracion, contenido, fecha):
+        self.valoracion = valoracion
+        self.contenido = contenido
+        self.fecha = fecha
+
