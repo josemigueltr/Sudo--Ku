d9a6c21c805a81c0437f3b6541849bf521d3425d
Vladimir Sierra <vladimirsierra@ciencias.unam.mx>
2021-07-26 18:47:09 -0500

    add comprador model

---
+++ back/models/comprador.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String
+from models.conexion_bd import Base
+
+class Comprador(Base):
+
+    __tablename__ = 'comprador'
+    username = Column(String, primary_key = True)
+    correo = Column(String)
+    telefono = Column(String)
+    contrasenia = Column(String)
+
+
+    def __init__(self, username, correo, telefono, contrasenia):
+        self.username = username
+        self.correo = correo
+        self.telefono = telefono
+        self.contrasenia = contrasenia
