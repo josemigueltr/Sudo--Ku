e70c75fd3eba4e2e956ac413af2c41ac599fc1bb
Daniel Castillo <danielcastillo59814@ciencias.unam.mx>
2021-07-26 18:47:54 -0500

    agregadas funcion inicializadora y funcion que parsea modelo producto a diccionario

--- back/models/producto.py
+++ back/models/producto.py
@@ -12,3 +12,26 @@ class Producto(Base):
     calificacion = Integer()
     stock = Integer()
     foto = String(100)
+
+    def __init__(self, username, nombre, descripcion, precio, calificacion, stock, foto):
+        self.username = username
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.precio = precio
+        self.calificacion = calificacion
+        self.stock = stock
+        self.foto = foto
+
+    def to_dict(self):
+        '''
+        regresa una representación del modelo en un diccionario
+        '''
+        return dict(
+            username=self.username,
+            nombre=self.nombre,
+            descripcion=self.descripcion,
+            precio=self.precio,
+            calificacion=self.calificacion,
+            stock=self.stock,
+            foto=self.foto
+        )
