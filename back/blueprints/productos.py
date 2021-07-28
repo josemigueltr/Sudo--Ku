49f3d33162642b01066067f902d4612ab31b078c
Daniel Castillo <danielcastillo59814@ciencias.unam.mx>
2021-07-28 16:51:22 -0500

    listo controlador de editar producto

--- back/blueprints/productos.py
+++ back/blueprints/productos.py
@@ -1,5 +1,15 @@
 # flask
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+
+# models
+from models.conexion_bd import Session
+from models.producto import Producto
+
+# aws
+from boto3.exceptions import S3UploadFailedError
+
+# utilities
+from utilities.amazon import upload_file
 
 bp = Blueprint('productos', __name__, url_prefix='/productos')
 
@@ -30,8 +40,26 @@ def agregar_producto(id):
 
 @bp.route('/<id>', methods=['PUT'])
 def editar_producto(id):
-  # TODO controlador: editar producto
-  pass
+  session = Session()
+  producto = session.query(Producto).get(id)
+  params = request.form
+  
+  producto.nombre = params['nombre'] if 'nombre' in params else producto.nombre
+  producto.descripcion = params['descripcion'] if 'descripcion' in params else producto.descripcion
+  producto.precio = params['precio'] if 'precio' in params else producto.precio
+  producto.calificacion = params['calificacion'] if 'calificacion' in params else producto.calificacion
+  producto.stock = params['stock'] if 'stock' in params else producto.stock
+
+  if 'foto' in request.files:
+    try:
+      producto.foto = upload_file(request.files['foto'])
+    except S3UploadFailedError as e:
+      return jsonify(dict(
+        message=str(e)
+      )), 500
+
+  session.commit()
+  return jsonify(producto.to_dict()), 200
 
 @bp.route('/<id>', methods=['DELETE'])
 def eliminar_producto(id):
