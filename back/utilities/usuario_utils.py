871e6ba2a24008eca2a9bc6de00c4d35dd85dbaa
josemigueltr <josemigueltr@ciencias.unam.mx>
2021-07-28 21:24:13 -0500

    Correccion envio de emails

--- back/utilities/usuario_utils.py
+++ back/utilities/usuario_utils.py
@@ -12,11 +12,14 @@ def generar_contrasenia():
 
 def envia_mail(datos,tipo):
     mail = Mail(app)
-    msg_registro=f"Hola!  {datos[1]} \n Gracias por registrarte como {datos[2]} en ebarrotes \n Esta es tu contraseña para que puedas ingresar al sistema {datos[3]}"
-    msg_compra=f"Hola!  {datos[1]} \n Gracias por realizar tu compra en ebarrotes \n El monto total fue de:{datos[2]}"
+    mensaje=""
+    if tipo=="registro":
+        mensaje=f"Hola!  {datos[1]} \n Gracias por registrarte como {datos[2]} en ebarrotes \n Esta es tu contraseña para que puedas ingresar al sistema {datos[3]}"
+    else:
+        mensaje=f"Hola!  {datos[1]} \n Gracias por realizar tu compra en ebarrotes \n El total de tu compra fue de {datos[2]} "
     try:
         msg = Message('Cuenta ebarrotes', sender =   f'{datos[0]}', recipients = [f'{datos[0]}'])
-        msg.body = msg_registro if tipo=="registro" else msg_compra
+        msg.body = mensaje
         mail.send(msg)
     except Exception:
         raise
