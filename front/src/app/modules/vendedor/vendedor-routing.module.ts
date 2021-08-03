e8b40f2c159a0a0b3129f5c1020bebb06bff9f00
Daniel Castillo <danielcastillo59814@ciencias.unam.mx>
2021-08-03 16:05:36 -0500

    agregando ruta de la pagina donde se muestran los productos de un vendedor

--- front/src/app/modules/vendedor/vendedor-routing.module.ts
+++ front/src/app/modules/vendedor/vendedor-routing.module.ts
@@ -1,7 +1,14 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaProductosComponent } from './pages/lista-productos/lista-productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListaProductosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
