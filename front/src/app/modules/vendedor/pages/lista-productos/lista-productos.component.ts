3dc61686b38e18b55643e3a2c81df6e47f1fc034
Vladimir Sierra <vladimirsierra@ciencias.unam.mx>
2021-08-04 19:37:23 -0500

    Add lista-productos.component.html view Still remaining: call the back to get the list of items

--- front/src/app/modules/vendedor/pages/lista-productos/lista-productos.component.ts
+++ front/src/app/modules/vendedor/pages/lista-productos/lista-productos.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaProductosComponent implements OnInit {
   // TODO vista: lista de productos de vendedor
-  constructor() { }
+  constructor() {
+
+  }
+
+  headElements = ['#', 'Nombre', 'Precio', 'Calificacion', 'Stock', 'Action'];
+
+  productos = [
+    {
+      "id_producto" : 1,
+      "nombre": "pencils",
+      "precio": 13.5,
+      "calificacion" : 4,
+      "stock": 5
+    },
+    {
+      "id_producto" : 2,
+      "nombre": "producto2",
+      "precio": 50,
+      "calificacion" : 10,
+      "stock": 1
+    }
+  ] ;
 
   ngOnInit(): void {
+
   }
 
 }
